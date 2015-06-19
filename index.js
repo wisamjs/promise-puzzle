285c3f00f7275a51f71e19d552fd91a7006e9a3d
wzaghal <wisamzaghal@gmail.com>
2015-06-18 21:05:48 -0400

    simple exercise

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 var Promise = require('bluebird');
+
+var url = 'site.com/api/product/';
+var requestIds = [105,234,1204, 152];
+
+var urlRequests = requestIds.map(function(id){
+  return url + id;
+});
+
+function apiCall(url){
+  console.log('PROCESSING: ' + url);
+  return new Promise(function(resolve) {
+    return setTimeout(function(){
+      resolve(200)
+    }, 3000);
+  });
+}
+
+
+/*
+ * Simulate single API call
+ */
+apiCall(urlRequests[0])
+.then(function(res){
+  console.log('Response: ' + res);
+
+})
+
+
