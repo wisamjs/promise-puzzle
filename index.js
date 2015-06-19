cec9a77af2a1c4bc5a2525c7676b114980d002f3
wzaghal <wisamzaghal@gmail.com>
2015-06-18 22:27:57 -0400

    sequential calls

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var Promise = require('bluebird');
 
 var url = 'site.com/api/product/';
-var requestIds = [105,234,1204, 152];
+var requestIds = [105, 234, 1204, 152];
 
 var urlRequests = requestIds.map(function(id){
   return url + id;
@@ -11,6 +11,7 @@ function apiCall(url){
   console.log('PROCESSING: ' + url);
   return new Promise(function(resolve) {
     return setTimeout(function(){
+  console.log('DONE: ' + url);
       resolve(200)
     }, 3000);
   });
@@ -23,6 +24,20 @@ function apiCall(url){
 apiCall(urlRequests[0])
 .then(function(res){
   console.log('Response: ' + res);
+
+})
+
+/*
+ * Sequentially call each request and get result
+ */
+Promise.reduce(urlRequests, function(total, req){
+  return apiCall(req)
+  .then(function(res){
+    return total+= 1;
+  })
+}, 0)
+.then(function(calls){
+  console.log('successful calls: ' + calls);
 
 })
 
